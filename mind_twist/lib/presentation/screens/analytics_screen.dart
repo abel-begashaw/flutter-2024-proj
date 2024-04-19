8be1aa4bc9f7b28e22ef632eda1e7ecc3746e996
Abel Begashaw <162702561+abel-begashaw@users.noreply.github.com>
2024-04-20 00:22:52 +0300

    Finished the routing for analytics_screen.dart

--- mind_twist/lib/presentation/screens/analytics_screen.dart
+++ mind_twist/lib/presentation/screens/analytics_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-class AnalyticsScreen extends StatelessWidget {
+class AnalyticsScreen extends StatefulWidget {
   const AnalyticsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AnalyticsScreen> createState() => _AnalyticsScreenState();
+}
+
+class _AnalyticsScreenState extends State<AnalyticsScreen> {
+  int _selectedIndex = 2;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      // Navigate to other screens based on index
+      switch (index) {
+        case 0:
+          Navigator.pushNamed(context, '/home_screen'); // Navigate to HomeScreen
+          break;
+        case 1:
+          Navigator.pushNamed(context, '/teaser'); // Navigate to TeasersScreen
+          break;
+        case 2:
+          // Navigate to AnalyticsScreen (current screen)
+          break;
+        case 3:
+          Navigator.pushNamed(context, '/profile'); // Navigate to ProfilePage
+          break;
+        default:
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
+      backgroundColor: const Color.fromARGB(255, 120, 113, 170),
+      // Your Scaffold contents here...
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,8 +56,8 @@ class AnalyticsScreen extends StatelessWidget {
                       // Implement action when the button is pressed
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white38, // Using the primary color of the app
-                      minimumSize: Size(double.infinity, 50), // Full width button
+                      backgroundColor: Colors.white,
+                      minimumSize: Size(double.infinity, 50),
                     ),
                     child: Text(
                       'Refresh Statistics',
@@ -53,28 +83,27 @@ class AnalyticsScreen extends StatelessWidget {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          _buildStatItem('Quizzes Taken:', '5'), // Replace with actual data
-                          _buildStatItem('Questions Answered:', '50'), // Replace with actual data
-                          _buildStatItem('Questions Missed:', '10'), // Replace with actual data
-                          const SizedBox(height: 20), // Added padding
+                          _buildStatItem('Quizzes Taken:', '5'),
+                          _buildStatItem('Questions Answered:', '50'),
+                          _buildStatItem('Questions Missed:', '10'),
+                          const SizedBox(height: 20),
                           const Divider(height: 20, color: Colors.grey),
-                          const SizedBox(height: 20), // Added padding
-                          _buildStatItem('Total Time Taken:', '2h 30m'), // Replace with actual data
-                          _buildStatItem('Average Time per Question:', '3m 15s'), // Replace with actual data
-                          _buildStatItem('Fastest Completion Time:', '1h 20m'), // Replace with actual data
-                          _buildStatItem('Slowest Completion Time:', '3h 10m'), // Replace with actual data
-                          const SizedBox(height: 20), // Added padding
+                          const SizedBox(height: 20),
+                          _buildStatItem('Total Time Taken:', '2h 30m'),
+                          _buildStatItem('Average Time per Question:', '3m 15s'),
+                          _buildStatItem('Fastest Completion Time:', '1h 20m'),
+                          _buildStatItem('Slowest Completion Time:', '3h 10m'),
+                          const SizedBox(height: 20),
                           const Divider(height: 20, color: Colors.grey),
-                          const SizedBox(height: 20), // Added padding
+                          const SizedBox(height: 20),
                           const Text(
                             'Additional Stats:',
                             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                           ),
-                          _buildStatItem('Accuracy Percentage:', '85%'), // Replace with actual data
-                          _buildStatItem('Average Score:', '75'), // Replace with actual data
-                          _buildStatItem('Highest Score:', '90'), // Replace with actual data
-                          _buildStatItem('Lowest Score:', '60'), // Replace with actual data
-                          // Add more relevant stats as needed
+                          _buildStatItem('Accuracy Percentage:', '85%'),
+                          _buildStatItem('Average Score:', '75'),
+                          _buildStatItem('Highest Score:', '90'),
+                          _buildStatItem('Lowest Score:', '60'),
                         ],
                       ),
                     ),
@@ -93,39 +122,22 @@ class AnalyticsScreen extends StatelessWidget {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.category_rounded),
-            label: 'Categories',
+            label: 'Teasers',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.question_mark_rounded),
-            label: 'Teasers',
+            icon: Icon(Icons.analytics_rounded),
+            label: 'Analytics',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.account_circle),
             label: 'Profile',
           ),
         ],
-        currentIndex: 3,
-        selectedItemColor: Colors.purple,
-        backgroundColor: Colors.white,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple, // Change color as needed
+        backgroundColor: Colors.white, // Change color as needed
         type: BottomNavigationBarType.fixed,
-        onTap: (index) {
-          // Handle navigation based on index
-          switch (index) {
-            case 0:
-              // Navigate to Home
-              break;
-            case 1:
-              // Navigate to Categories
-              break;
-            case 2:
-              // Navigate to Teasers
-              break;
-            case 3:
-              // Already on Profile page
-              break;
-            default:
-          }
-        },
+        onTap: _onItemTapped,
       ),
     );
   }
@@ -146,4 +158,3 @@ class AnalyticsScreen extends StatelessWidget {
     );
   }
 }
-
