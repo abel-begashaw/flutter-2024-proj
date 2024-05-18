2aad82c2be21ea5a51e89faf82837f53c423d623
Naol Kumela <j45674190@gmail.com>
2024-05-18 07:10:01 +0300

    restructured the folder structure in adherence to DDD principles added core dir and main

--- mind_twist/lib/main.dart
+++ mind_twist/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mind_twist/presentation/screens/admin.dart';
-import 'package:mind_twist/presentation/screens/analytics_screen.dart';
-import 'package:mind_twist/presentation/screens/landing_page.dart';
-import 'package:mind_twist/presentation/screens/home_screen.dart';
-import 'package:mind_twist/presentation/screens/profile.dart';
-import 'package:mind_twist/presentation/screens/signIn.dart';
-import 'package:mind_twist/presentation/screens/signUp.dart';
-import 'package:mind_twist/presentation/screens/teaser_screen.dart';
-import 'package:mind_twist/presentation/screens/update_profile.dart';
+import 'package:mind_twist/presentation/screens/admin/admin.dart';
+import 'package:mind_twist/presentation/screens/teaser/analytics_screen.dart';
+import 'package:mind_twist/presentation/screens/welcome/landing_page.dart';
+import 'package:mind_twist/presentation/screens/home/home_screen.dart';
+import 'package:mind_twist/presentation/screens/profile/profile.dart';
+import 'package:mind_twist/presentation/screens/welcome/signIn.dart';
+import 'package:mind_twist/presentation/screens/welcome/signUp.dart';
+import 'package:mind_twist/presentation/screens/teaser/teaser_screen.dart';
+import 'package:mind_twist/presentation/screens/profile/update_profile.dart';
 
 void main() {
   runApp(const MyApp());
